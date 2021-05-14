2e6e2bd2fc9f94ac96e588719aa5c0baf94ea8a9
Yadier <yadierwww@gmail.com>
2021-05-13 23:29:40 -0500

    Se agrega el arrito

---
+++ src/componets/Carrito.js
@@ -0,0 +1,37 @@
+import React from 'react'
+let storage = []
+if (localStorage.getItem('storage')) {
+    storage = JSON.parse(localStorage.getItem('storage'))
+}
+const Carrito = () => {
+    return (
+        <>
+            <div>
+                <div> <i className="fas fa-arrow-left"></i> </div>
+                <div>Carrito</div>
+                {
+                    storage.map(item =>
+                    (
+                        
+                            <div key={item.idArticulo}>
+                                <div><img src={item.foto} alt="" width={'150px'} /></div>
+                                <div>{"guajola de tamal verde"}</div>
+                                <div>${345} MXN</div>
+                            </div>
+
+                        
+                    ))
+                }
+                <div>
+                    <p style={{ background: "red", padding:'20px' } } onClick={
+                        () => {
+                            alert("aqui sigue el chekout")
+                        }
+                    }>Pagar</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Carrito
